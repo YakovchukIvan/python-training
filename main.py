135b18c931c5bad26349bdddf5fb93846b3cfe3d
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-07-12 21:15:39 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# city = input("Enter your city: ")
+
+
+# print([name, age, city])
+# print(name)
+
+# print(dir(name))
+# def my_name(name):
+#   print(name)
+
+# my_name('Ivan')
+
+my_list = [1, 22]
+
+print(my_list)
+''
