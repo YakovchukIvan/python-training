3455308d2b660b59a8cca94e9e1a9412d94249ed
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-07-13 16:41:14 +0300

    passed 4 more module

--- main.py
+++ main.py
@@ -2,17 +2,35 @@
 # age = input("Enter your age: ")
 # city = input("Enter your city: ")
 
-
 # print([name, age, city])
 # print(name)
 
 # print(dir(name))
+
 # def my_name(name):
-#   print(name)
+#     print(name)
 
 # my_name('Ivan')
+# my_list = [1, 22]
+# print(my_list)
 
-my_list = [1, 22]
+# a = 10
 
-print(my_list)
-''
+# def my_fn():
+#     print("hello")
+
+# print("ivan")
+
+# def hello(name):
+#     res = ("Hello there!", name)
+#     return res
+
+
+# first = hello('Ivan')
+# print(first)
+
+# print(hello('World'))
+
+# import datetime
+
+# print(datetime.MAXYEAR)
