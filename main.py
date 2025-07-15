db354b719f2d9a5f86d6d1183aa7142b2d7410ba
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2025-07-15 08:41:18 +0300

    completed modules 10 to 13

--- main.py
+++ main.py
@@ -34,3 +34,88 @@
 # import datetime
 
 # print(datetime.MAXYEAR)
+
+# my_name = 'Ivan'
+# print(id(my_name))
+
+# my_num = 100
+# print(id(my_num))
+# print(id(my_num))
+
+
+# info_msg = """You
+# are learning the easiest
+# programming language -
+# Python!"""
+
+# print(info_msg)
+
+# print(type(info_msg))
+
+
+# my_name = 'Ivan Yakovchuk'
+
+# print(len(my_name))
+
+# print(my_name[:4])
+
+# input_str = input("Enter any number: ")
+
+# input_int = int(input_str)
+
+# print(input_int)
+
+# print(type(input_int))
+
+# base = 5
+# power = 3.2
+# step_number = pow(base, power)
+
+# print(step_number)
+# print(type(step_number))
+
+# step_number = int(step_number)
+
+# print(step_number)
+# print(type(step_number))
+
+# long_int = 1_000_000_000
+
+# print(long_int)
+
+# average_price = 28.75
+# price = int(average_price)
+
+# print(price)           # 28
+# print(type(price))     # <class 'int'>
+
+
+# str_temperature = '14.5'
+# temperature = float(str_temperature)
+
+# print(temperature)        # 14.5
+# print(type(temperature))  # <class 'float'>
+
+# my_name = 'Ivan Yakovchuk'
+# bool(my_name)
+# print(type(my_name))
+
+
+# db_is_available = False
+
+# print(db_is_available)
+# print(type(db_is_available))
+
+# db_is_available = True
+# print(db_is_available)
+
+# print(bool(10))
+# print(bool('abc'))
+# print(bool([]))
+# print(bool([1, 2]))
+# print(bool(None))
+
+# print(100 > 10)
+# print("Long string" > 'Short')
+# print([] == [])
+# print({'a': 3} == {'a': 3})
